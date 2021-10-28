8e4e18468af0c6d4cbcc8ae3ca45dea8d1c8270b
diego-bernardesb <diegobbabo@gmail.com>
2021-10-28 10:33:14 -0300

    Iniciação do projeto app-Ecomerce

---
+++ src/main/java/com/diiegob/appecomerce/AppEcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.diiegob.appecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEcomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diiegob/appecomerce/AppEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diiegob.appecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
